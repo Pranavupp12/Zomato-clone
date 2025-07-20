697125c4cb5b92a39723376fa569bce6c545ec85
Pranavupp12 <165937783+Pranavupp12@users.noreply.github.com>
2025-07-21 02:40:54 +0530

    Add files via upload

---
+++ admin /src/pages/List/List.jsx
@@ -0,0 +1,73 @@
+import React, { use, useState } from "react";
+import "./List.css";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useEffect } from "react";
+
+
+const List = ({url}) => {
+
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    const response = await axios.get(`${url}/api/food/list`);
+    console.log(response.data);
+    if (response.data.success) {
+      setList(response.data.data);
+    } else {
+      toast.error("error");
+    }
+  };
+
+  const removeFood = async (foodId) => {
+    try {
+      const response = await axios.post(`${url}/api/food/remove`, {
+        id: foodId,
+      });
+
+      if (response.data.success) {
+        toast.success("Food item removed!");
+        await fetchList();
+      } else {
+        toast.error("Failed to remove food");
+      }
+    } catch (error) {
+      toast.error("Server error while removing food");
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
+  return (
+    <div className="list add flex-col">
+      <p>All Foods List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.map((item, index) => {
+          return (
+            <div key={index} className="list-table-format">
+              <img src={`${url}/images/` + item.image} alt="" />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>${item.price}</p>
+              <p onClick={() => removeFood(item._id)} className="cursor">
+                X
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
